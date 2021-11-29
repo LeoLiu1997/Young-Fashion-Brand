e2401fd47e00f6762c73a01b3af60299b8b9241f
Yongyao Liu <91501569+LeoLiu1997@users.noreply.github.com>
2021-11-29 06:16:18 +0000

    V2

---
+++ script.js
@@ -0,0 +1,31 @@
+$('.supreme-card').click(function(){
+  $('.supreme-card').addClass('supreme-card-expand')
+  var delayInMilliseconds = 2000; //1 second
+  setTimeout(function() {
+    window.location.href = "supreme.html";
+  }, delayInMilliseconds);
+})
+
+$('.jordan-card').click(function(){
+  $('.jordan-card').addClass('jordan-card-expand')
+  var delayInMilliseconds = 2000; //1 second
+  setTimeout(function() {
+    window.location.href = "jordan.html";
+  }, delayInMilliseconds);
+})
+
+$('.kith-card').click(function(){
+  $('.kith-card').addClass('kith-card-expand')
+  var delayInMilliseconds = 2000; //1 second
+  setTimeout(function() {
+    window.location.href = "kith.html";
+  }, delayInMilliseconds);
+})
+
+$('.yeezy-card').click(function(){
+  $('.yeezy-card').addClass('yeezy-card-expand')
+  var delayInMilliseconds = 2000; //1 second
+  setTimeout(function() {
+    window.location.href = "yeezy.html";
+  }, delayInMilliseconds);
+})
